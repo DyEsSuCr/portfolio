c70658906ad101aca022c1bc83ae7c52ebb74a82
DylanSuarez <2000sajo@gmail.com>
2023-03-31 22:04:04 -0500

    about

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#101320',
+        grayy: '#333643',
+        bluee: '#3182ce'
+      }
+    }
+  },
+  plugins: []
+}
